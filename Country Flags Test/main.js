5e1e06e1551994c64292e3af95acf2011737b0af
ErnestUstian <113203260+ErnestUstian@users.noreply.github.com>
2023-02-15 13:38:16 -0500

    gl

--- Country Flags Test/main.js
+++ Country Flags Test/main.js
@@ -1,11 +1,27 @@
 import {DOM} from "./DOM"
 var flag = undefined
 
-async function displayFlag() {
+async function getFlag() {
   let response = await fetch("https://restcountries.com/v3.1/all")
   let flags = await response.json()
   let index = Math.floor(Math.random()*flags.length)
   console.log(flags[index])
 }
 
-displayFlag()
+function display{
+  DOM.body.innerHTML = "";
+  food.forEach((flags) =>
+    DOM.insertAdjacentHTML(
+      "beforeend",
+      `
+    <div class="card">
+        <h1>${flags.name}</h1>
+        <h2>${flags.status}</h2>
+        <img src="${flags.image}" alt="image of ${flag.name}">
+    </div>
+    `
+    )
+  );
+}
+
+getFlag()
