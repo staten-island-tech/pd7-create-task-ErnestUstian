3ce7980027e26d1634cacd6c72b73b7203d717ad
ErnestUstian <113203260+ErnestUstian@users.noreply.github.com>
2023-02-16 22:45:07 -0500

    hfodngj

--- Country Flags Test/main.js
+++ Country Flags Test/main.js
@@ -1,23 +1,23 @@
-import {DOM}from "./DOM"
+import { DOM } from "./DOM";
 
 async function getFlag() {
-  let response = await fetch("https://restcountries.com/v3.1/all")
-  let flags = await response.json()
-  let index = Math.floor(Math.random()*flags.length)
-  console.log(flags[index])
-  display(flags[index])
+  let response = await fetch("https://restcountries.com/v3.1/all");
+  let flags = await response.json();
+  let index = Math.floor(Math.random() * flags.length);
+  console.log(flags[index]);
+  display(flags[index]);
 }
 
 function display(flag) {
   DOM.Menupage.innerHTML = "";
-    DOM.Menupage.insertAdjacentHTML(
-      "beforeend",
-      `
+  DOM.Menupage.insertAdjacentHTML(
+    "beforeend",
+    `
     <div class="card">
-        <img src="${flag.flags.png}" alt="image of ${flag.flags.alt}">
+        <img src="${flag.flags.png}" alt="${flag.flags.alt}">
     </div>
     `
-    );
+  );
 }
 
-getFlag()
+getFlag();
