7552fcf0047ce2517b8871a6520d4b32e5b4ec05
ErnestUstian <113203260+ErnestUstian@users.noreply.github.com>
2023-02-17 14:00:28 -0500

    gfjgf

--- Country Flags Test/main.js
+++ Country Flags Test/main.js
@@ -2,6 +2,7 @@ import { DOM } from "./DOM";
 
 async function getFlag() {
   let response = await fetch("https://restcountries.com/v3.1/all");
+  
   let flags = await response.json();
   let index = Math.floor(Math.random() * flags.length);
   console.log(flags[index]);
