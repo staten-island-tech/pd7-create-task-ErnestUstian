3b985965953217160ca1fa138df35072541f95a8
ErnestUstian <113203260+ErnestUstian@users.noreply.github.com>
2023-02-18 15:13:38 -0500

    gdffgj

--- Country Flags Test/main.js
+++ Country Flags Test/main.js
@@ -2,13 +2,21 @@ import { DOM } from "./DOM";
 
 async function getFlag() {
   let response = await fetch("https://restcountries.com/v3.1/all");
-  
   let flags = await response.json();
-  let index = Math.floor(Math.random() * flags.length);
-  console.log(flags[index]);
-  display(flags[index]);
+
+  //Filter independent countries
+  const filteredCountries = flags.filter(
+    (country) =>
+      country.independent === true && country.independent !== undefined
+  );
+
+  //Get random flag from filteredCountries
+  let index = Math.floor(Math.random() * filteredCountries.length);
+  console.log(filteredCountries[index]);
+  display(filteredCountries[index]);
 }
 
+//Display Flag
 function display(flag) {
   DOM.Menupage.innerHTML = "";
   DOM.Menupage.insertAdjacentHTML(
